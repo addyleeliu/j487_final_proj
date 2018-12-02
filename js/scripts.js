5b5f9c34064de750d4ea3b608a0cf338a772361d
Addy <addyleeliu@gmail.com>
2018-12-02 16:20:43 -0500

    added query for state population counts

--- js/scripts.js
+++ js/scripts.js
@@ -4,28 +4,52 @@ $(function(){
 
   var censusKey = config.CENSUS_KEY;
 
-  var boaaURL = 'https://api.census.gov/data/2010/sf1?get=P012B001&for=state:*&key=' + censusKey;
+  var statePopURL = 'https://api.census.gov/data/2010/sf1?get=P0010001,NAME&for=state:*&key=' + censusKey;
+
   var whiteURL = 'https://api.census.gov/data/2010/sf1?get=P012A001&for=state:*&key=' + censusKey;
+  var boaaURL = 'https://api.census.gov/data/2010/sf1?get=P012B001&for=state:*&key=' + censusKey;
   var asianURL = 'https://api.census.gov/data/2010/sf1?get=P012D001&for=state:*&key=' + censusKey;
   var aianURL = 'https://api.census.gov/data/2010/sf1?get=P012C001&for=state:*&key=' + censusKey;
   var nhpiURL = 'https://api.census.gov/data/2010/sf1?get=P012E001&for=state:*&key=' + censusKey;
   var otherRaceURL = 'https://api.census.gov/data/2010/sf1?get=P012F001&for=state:*&key=' + censusKey;
   var multiRaceURL = 'https://api.census.gov/data/2010/sf1?get=P012G001&for=state:*&key=' + censusKey;
-  var query = '';
+
+  var censusData;
   var sum;
-  var raceData;
+
+  // CALL TO US CENSUS API FOR POPULATION TOTAL OF EACH STATE
+  $.ajax({
+    type: 'GET',
+    dataType: 'json',
+    data: censusData,
+    url: statePopURL,
+    success: function(censusData) {
+      console.log("(From US Census API) 2010 per-state population: " + censusData);
+      sum = 0;
+      for (var i = 1; i < censusData.length-1; i++) { /* excludes first descriptive title array & Puerto Rico data */
+        sum += Number(censusData[i][0]);
+      }
+      console.log("(From US Census API) 2010 total population: " + sum);
+    },
+    error: function(msg) {
+      console.log('state population data did not load!');
+    },
+    complete: function(msg) {
+      console.log('AJAX request for state population counts completed!')
+    }
+  }); // CLOSE CALL TO US CENSUS API FOR POPULATION TOTAL OF EACH STATE
 
   // CALL TO US CENSUS API FOR POPULATION COUNTS OF WHITE PEOPLE
   $.ajax({
     type: 'GET',
     dataType: 'json',
-    data: raceData,
+    data: censusData,
     url: whiteURL,
-    success: function(raceData) {
-      console.log("(From US Census API) 2010 per-state population of people who are white alone: " + raceData);
+    success: function(censusData) {
+      console.log("(From US Census API) 2010 per-state population of people who are white alone: " + censusData);
       sum = 0;
-      for (var i = 1; i < raceData.length-1; i++) { /* excludes first descriptive title array & Puerto Rico data */
-        sum += Number(raceData[i][0]);
+      for (var i = 1; i < censusData.length-1; i++) { /* excludes first descriptive title array & Puerto Rico data */
+        sum += Number(censusData[i][0]);
       }
       console.log("(From US Census API) 2010 total population of people who are white alone: " + sum);
     },
@@ -41,13 +65,13 @@ $(function(){
   $.ajax({
     type: 'GET',
     dataType: 'json',
-    data: raceData,
+    data: censusData,
     url: boaaURL,
-    success: function(raceData) {
-      console.log("(From US Census API) 2010 per-state population of people who are Black or African American alone: " + raceData);
+    success: function(censusData) {
+      console.log("(From US Census API) 2010 per-state population of people who are Black or African American alone: " + censusData);
       sum = 0;
-      for (var i = 1; i < raceData.length-1; i++) { /* excludes first descriptive title array & Puerto Rico data */
-        sum += Number(raceData[i][0]);
+      for (var i = 1; i < censusData.length-1; i++) { /* excludes first descriptive title array & Puerto Rico data */
+        sum += Number(censusData[i][0]);
       }
       console.log("(From US Census API) 2010 total population of people who are Black or African American alone: " + sum);
     },
@@ -63,13 +87,13 @@ $(function(){
   $.ajax({
     type: 'GET',
     dataType: 'json',
-    data: raceData,
+    data: censusData,
     url: asianURL,
-    success: function(raceData) {
-      console.log("(From US Census API) 2010 per-state population of people who are Asian alone: " + raceData);
+    success: function(censusData) {
+      console.log("(From US Census API) 2010 per-state population of people who are Asian alone: " + censusData);
       sum = 0;
-      for (var i = 1; i < raceData.length-1; i++) { /* excludes first descriptive title array & Puerto Rico data */
-        sum += Number(raceData[i][0]);
+      for (var i = 1; i < censusData.length-1; i++) { /* excludes first descriptive title array & Puerto Rico data */
+        sum += Number(censusData[i][0]);
       }
       console.log("(From US Census API) 2010 total population of people who are Asian alone: " + sum);
     },
@@ -85,13 +109,13 @@ $(function(){
   $.ajax({
     type: 'GET',
     dataType: 'json',
-    data: raceData,
+    data: censusData,
     url: aianURL,
-    success: function(raceData) {
-      console.log("(From US Census API) 2010 per-state population of people who are American Indian and Alaska Native alone: " + raceData);
+    success: function(censusData) {
+      console.log("(From US Census API) 2010 per-state population of people who are American Indian and Alaska Native alone: " + censusData);
       sum = 0;
-      for (var i = 1; i < raceData.length-1; i++) { /* excludes first descriptive title array & Puerto Rico data */
-        sum += Number(raceData[i][0]);
+      for (var i = 1; i < censusData.length-1; i++) { /* excludes first descriptive title array & Puerto Rico data */
+        sum += Number(censusData[i][0]);
       }
       console.log("(From US Census API) 2010 total population of people who are American Indian and Alaska Native alone: " + sum);
     },
@@ -107,13 +131,13 @@ $(function(){
   $.ajax({
     type: 'GET',
     dataType: 'json',
-    data: raceData,
+    data: censusData,
     url: nhpiURL,
-    success: function(raceData) {
-      console.log("(From US Census API) 2010 per-state population of people who are Native Hawaiian and Other Pacific Islander alone: " + raceData);
+    success: function(censusData) {
+      console.log("(From US Census API) 2010 per-state population of people who are Native Hawaiian and Other Pacific Islander alone: " + censusData);
       sum = 0;
-      for (var i = 1; i < raceData.length-1; i++) { /* excludes first descriptive title array & Puerto Rico data */
-        sum += Number(raceData[i][0]);
+      for (var i = 1; i < censusData.length-1; i++) { /* excludes first descriptive title array & Puerto Rico data */
+        sum += Number(censusData[i][0]);
       }
       console.log("(From US Census API) 2010 total population of people who are Native Hawaiian and Other Pacific Islander alone: " + sum);
     },
@@ -129,13 +153,13 @@ $(function(){
   $.ajax({
     type: 'GET',
     dataType: 'json',
-    data: raceData,
+    data: censusData,
     url: otherRaceURL,
-    success: function(raceData) {
-      console.log("(From US Census API) 2010 per-state population of people who are of some other race: " + raceData);
+    success: function(censusData) {
+      console.log("(From US Census API) 2010 per-state population of people who are of some other race: " + censusData);
       sum = 0;
-      for (var i = 1; i < raceData.length-1; i++) { /* excludes first descriptive title array & Puerto Rico data */
-        sum += Number(raceData[i][0]);
+      for (var i = 1; i < censusData.length-1; i++) { /* excludes first descriptive title array & Puerto Rico data */
+        sum += Number(censusData[i][0]);
       }
       console.log("(From US Census API) 2010 total population of people who are of some other race: " + sum);
     },
@@ -151,13 +175,13 @@ $(function(){
   $.ajax({
     type: 'GET',
     dataType: 'json',
-    data: raceData,
+    data: censusData,
     url: multiRaceURL,
-    success: function(raceData) {
-      console.log("(From US Census API) 2010 per-state population of people of two or more races: " + raceData);
+    success: function(censusData) {
+      console.log("(From US Census API) 2010 per-state population of people of two or more races: " + censusData);
       sum = 0;
-      for (var i = 1; i < raceData.length-1; i++) { /* excludes first descriptive title array & Puerto Rico data */
-        sum += Number(raceData[i][0]);
+      for (var i = 1; i < censusData.length-1; i++) { /* excludes first descriptive title array & Puerto Rico data */
+        sum += Number(censusData[i][0]);
       }
       console.log("(From US Census API) 2010 total population of people of two or more races: " + sum);
     },
